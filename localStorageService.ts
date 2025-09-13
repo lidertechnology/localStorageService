6fc52a7babeaf51ca1a068e8f40ea6589ef3daaf
Lider Technology <lidertech.net@gmail.com>
2025-09-13 11:26:04 -0500

    Update localStorageService.ts

--- localStorageService.ts
+++ localStorageService.ts
@@ -1,34 +1,25 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root'})
+
 export class LocalStorageService {
 
-  public setItem<T>(key: string, value: T): void {
-    try {
-      const serializedValue = JSON.stringify(value);
-      localStorage.setItem(key, serializedValue);
-    } catch (e) {
-      console.error('Error saving to localStorage', e);
-    }
+  /*** Guarda cualquier tipo de dato en el navegador. * La 'clave' es como el nombre del archivo, y el 'valor' es el contenido.  */
+   public guardarItem<T>(clave: string, valor: T): void {
+    try { const valorSerializado = JSON.stringify(valor); localStorage.setItem(clave, valorSerializado); } 
+    catch (e) { console.error('Error al guardar en el almacenamiento local', e); }
   }
 
-  public getItem<T>(key: string): T | null {
-    try {
-      const serializedValue = localStorage.getItem(key);
-      return serializedValue ? JSON.parse(serializedValue) as T : null;
-    } catch (e) {
-      console.error('Error getting from localStorage', e);
-      return null;
-    }
+  /** Recupera un dato guardado usando su 'clave'. * Si no lo encuentra, devuelve nulo.  */
+  public obtenerItem<T>(clave: string): T | null {
+    try { const valorSerializado = localStorage.getItem(clave); return valorSerializado ? JSON.parse(valorSerializado) as T : null;  } 
+    catch (e) {console.error('Error al obtener del almacenamiento local', e); return null; }
   }
 
-  public removeItem(key: string): void {
-    try {
-      localStorage.removeItem(key);
-    } catch (e) {
-      console.error('Error removing from localStorage', e);
-    }
+  /** Borra un dato guardado usando su 'clave'. */
+  public eliminarItem(clave: string): void {
+    try {localStorage.removeItem(clave); } 
+    catch (e) { console.error('Error al eliminar del almacenamiento local', e);  }
   }
+  
 }
